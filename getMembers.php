59c54bdf47a8e346fba28346c30213512e20a1c8
lou <louplebani@outlook.com>
2021-01-19 17:33:55 -0500

    working

--- getMembers.php
+++ getMembers.php
@@ -1,12 +1,13 @@
 <?php
+echo "TRYING";
 
 // Connecting, selecting database
-// $dbconn = pg_connect(
-//     "host=localhost 
-//     dbname=publishing 
-//     user=www 
-//     password=foo")
-//     or die('Could not connect: ' . pg_last_error());
+$dbconn = pg_connect(
+    "host=ec2-54-159-107-189.compute-1.amazonaws.com 
+    dbname=d4vupmidnt9lio 
+    user=igqywybsdluyqg 
+    password=7249b272a62aa3dd8811627fb3d1be4dfe052d015bd6c162834bcb92fe844598")
+    or die('Could not connect: ' . pg_last_error());
 
 // // Performing SQL query
 // $query = 'SELECT * FROM authors';
@@ -27,7 +28,7 @@
 // pg_free_result($result);
 
 // Closing connection
-// pg_close($dbconn);
+pg_close($dbconn);
 
 echo "SUCCESS SUCCESS SUCCESS ";
 ?>
