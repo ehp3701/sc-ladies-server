893387902372251deca3ce273a04d26eb192bf9a
lou <louplebani@outlook.com>
2021-01-19 17:54:22 -0500

    working

--- getMembers.php
+++ getMembers.php
@@ -1,16 +1,17 @@
 <?php
+echo "TRYING";
 // Connecting, selecting database
-$dbconn = pg_connect(
-    "host=ec2-54-159-107-189.compute-1.amazonaws.com
-    dbname=d4vupmidnt9lio
-    user=igqywybsdluyqg
-    password=7249b272a62aa3dd8811627fb3d1be4dfe052d015bd6c162834bcb92fe844598"
-)
-    or die('Could not connect: ' . pg_last_error());
+// $dbconn = pg_connect(
+//     "host=ec2-54-159-107-189.compute-1.amazonaws.com
+//     dbname=d4vupmidnt9lio
+//     user=igqywybsdluyqg
+//     password=7249b272a62aa3dd8811627fb3d1be4dfe052d015bd6c162834bcb92fe844598"
+// )
+//     or die('Could not connect: ' . pg_last_error());
 
 // Performing SQL query
-$query = 'SELECT * FROM members';
-$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+// $query = 'SELECT * FROM members';
+// $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
 // if (mysqli_num_rows($result) > 0) {
 //     echo "greater than zero";
@@ -24,8 +25,11 @@ $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 // }
 
 // Free resultset
-pg_free_result($result);
+// pg_free_result($result);
 
 // Closing connection
-pg_close($dbconn);
+// pg_close($dbconn);
+
+
+echo "<br>SUCCESS";
 ?>
