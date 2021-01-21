d86806aa64411b7a2b50a0cd4fa421c3e45f433d
lou <louplebani@outlook.com>
2021-01-21 16:36:51 -0500

    working

--- postGame.php
+++ postGame.php
@@ -2,25 +2,32 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-type: application/json');
 
-$gamedesc = $_POST('gamedesc');
-$teamevent = $_POST('teamevent');
+$json = file_get_contents('php://input'); 
+$data = json_decode($json); 
 
-$dbconn = pg_connect(
-    "host=ec2-18-208-49-190.compute-1.amazonaws.com
-    dbname=d6fv2q17lqpnqe
-    user=hlqlwhlsanjvke
-    password=0b87775eab64e77490e65d6c639b9ddf782e62e3acccb0017d301dab571c0eb7"
-)
-    or die('Could not connect: ' . pg_last_error());
+$gamedesc = $data->gamedesc;
+$teamevent = $data->teamevent;
 
-$sql = "INSERT INTO gamedesc (gamedesc, teamevent)  VALUES ('$gamedesc', '$teamevent')";
+// $dbconn = pg_connect(
+//     "host=ec2-18-208-49-190.compute-1.amazonaws.com
+//     dbname=d6fv2q17lqpnqe
+//     user=hlqlwhlsanjvke
+//     password=0b87775eab64e77490e65d6c639b9ddf782e62e3acccb0017d301dab571c0eb7"
+// )
+//     or die('Could not connect: ' . pg_last_error());
 
-if ($result = pg_query($sql)) {
-    echo "Data Added Successfully.";
-} else {
-    echo "Error.";
-}
+// $sql = "INSERT INTO gamedesc (gamedesc, teamevent)  VALUES ('$gamedesc', '$teamevent')";
 
-pg_close($dbconn);
+// if ($result = pg_query($sql)) {
+//     echo "Data Added Successfully.";
+// } else {
+//     echo "Error.";
+// }
+
+// pg_close($dbconn);
+
+echo "gamedesc = $gamedesc <br>";
+echo "teamevent = $teamevent <br>";
+
 ?>
 ?>
