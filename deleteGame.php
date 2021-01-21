6ab3062a884e43d7a50942b4c0925e1eb1dc3bf0
lou <louplebani@outlook.com>
2021-01-21 17:52:30 -0500

    working

--- deleteGame.php
+++ deleteGame.php
@@ -2,37 +2,29 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-type: application/json');
 
+$key = $_GET["key"];
 
-$gamekey = $_GET("gamekey");
+$dbconn = pg_connect(
+    "host=ec2-18-208-49-190.compute-1.amazonaws.com
+    dbname=d6fv2q17lqpnqe
+    user=hlqlwhlsanjvke
+    password=0b87775eab64e77490e65d6c639b9ddf782e62e3acccb0017d301dab571c0eb7"
+)
+    or die('Could not connect: ' . pg_last_error());
 
-echo "Key is $gamekey";
+$query = "DELETE FROM gamedesc WHERE key=$key";
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
+if (pg_num_rows($result) > 0) {
+    $emparray = array();
+    while ($row = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+        array_push($emparray, $row);
+    }
+    echo json_encode($emparray);
+} else {
+    echo "0 results";
+}
 
-// $dbconn = pg_connect(
-//     "host=ec2-18-208-49-190.compute-1.amazonaws.com
-//     dbname=d6fv2q17lqpnqe
-//     user=hlqlwhlsanjvke
-//     password=0b87775eab64e77490e65d6c639b9ddf782e62e3acccb0017d301dab571c0eb7"
-// )
-//     or die('Could not connect: ' . pg_last_error());
+pg_free_result($result);
 
-// $query = "DELETE FROM gamedesc WHERE key=$key";
-
-// echo $sql;
-
-// $result = pg_query($query) or die('Query failed: ' . pg_last_error());
-
-// if (pg_num_rows($result) > 0) {
-//     $emparray = array();
-//     while ($row = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-//         array_push($emparray, $row);
-//     }
-//     echo json_encode($emparray);
-// } else {
-//     echo "0 results";
-// }
-
-// pg_free_result($result);
-
-// pg_close($dbconn);
-?>
+pg_close($dbconn);
