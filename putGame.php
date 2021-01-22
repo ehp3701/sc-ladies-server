4020c9d710a644b81dbc82c06a4f44603f5fb5c2
lou <louplebani@outlook.com>
2021-01-22 12:53:29 -0500

    working

--- putGame.php
+++ putGame.php
@@ -14,26 +14,19 @@ $teamevent = $data->teamevent;
 $sql = "UPDATE gamedesc  SET gamedesc = '$gamedesc', teamevent = '$teamevent' WHERE key = $key";
 echo $sql;
 
-// $key = $data->key;
-// $gamedesc = $data->gamedesc;
-// $teamevent = $data->teamevent;
+$dbconn = pg_connect(
+    "host=ec2-18-208-49-190.compute-1.amazonaws.com
+    dbname=d6fv2q17lqpnqe
+    user=hlqlwhlsanjvke
+    password=0b87775eab64e77490e65d6c639b9ddf782e62e3acccb0017d301dab571c0eb7"
+)
+    or die('Could not connect: ' . pg_last_error());
 
-// $dbconn = pg_connect(
-//     "host=ec2-18-208-49-190.compute-1.amazonaws.com
-//     dbname=d6fv2q17lqpnqe
-//     user=hlqlwhlsanjvke
-//     password=0b87775eab64e77490e65d6c639b9ddf782e62e3acccb0017d301dab571c0eb7"
-// )
-//     or die('Could not connect: ' . pg_last_error());
+if ($result = pg_query($sql)) {
+    echo "Data Updated Successfully.";
+} else {
+    echo "Error.";
+}
 
-// $sql = "UPDATE gamedesc  SET gamedesc = $gamedesc, teamevent = $teamevent WHERE key = $key";
-
-// if ($result = pg_query($sql)) {
-//     echo "Data Updated Successfully.";
-// } else {
-//     echo "Error.";
-// }
-
-echo $sql;
 
 pg_close($dbconn);
