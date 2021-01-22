474372863648aa2043dd53b4be241ca00f516377
lou <louplebani@outlook.com>
2021-01-22 11:28:02 -0500

    working

--- putGame.php
+++ putGame.php
@@ -2,25 +2,27 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-type: application/json');
 
-$json = file_get_contents('php://input');
-$data = json_decode($json);
+echo "WORKING WORKING";
 
-echo "json";
-echo $data;
+// $json = file_get_contents('php://input');
+// $data = json_decode($json);
 
-$key = $data->key;
-$gamedesc = $data->gamedesc;
-$teamevent = $data->teamevent;
+// echo "json";
+// echo $data;
 
-$dbconn = pg_connect(
-    "host=ec2-18-208-49-190.compute-1.amazonaws.com
-    dbname=d6fv2q17lqpnqe
-    user=hlqlwhlsanjvke
-    password=0b87775eab64e77490e65d6c639b9ddf782e62e3acccb0017d301dab571c0eb7"
-)
-    or die('Could not connect: ' . pg_last_error());
+// $key = $data->key;
+// $gamedesc = $data->gamedesc;
+// $teamevent = $data->teamevent;
 
-$sql = "UPDATE gamedesc  SET gamedesc = $gamedesc, teamevent = $teamevent WHERE key = $key";
+// $dbconn = pg_connect(
+//     "host=ec2-18-208-49-190.compute-1.amazonaws.com
+//     dbname=d6fv2q17lqpnqe
+//     user=hlqlwhlsanjvke
+//     password=0b87775eab64e77490e65d6c639b9ddf782e62e3acccb0017d301dab571c0eb7"
+// )
+//     or die('Could not connect: ' . pg_last_error());
+
+// $sql = "UPDATE gamedesc  SET gamedesc = $gamedesc, teamevent = $teamevent WHERE key = $key";
 
 // if ($result = pg_query($sql)) {
 //     echo "Data Updated Successfully.";
